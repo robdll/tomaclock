8655abdff77c0a69c8549c642b0e6d73311284e9
roberto di lillo <dilillo.roberto@gmail.com>
2022-05-20 20:48:42 +0100

    adds basic tomato settins

---
+++ src/components/Tomato/Tomato.js
@@ -0,0 +1,52 @@
+import './Tomato.css';
+import { useState } from 'react';
+
+function Tomato() {
+    
+    const [settings, setSettings] = useState({session: 25, break: 5});
+    
+    const editSettings =  (type, val) => {
+        const newSettings = { ...settings }
+        newSettings[type] +=val;
+        if(newSettings[type] > 59) {
+            newSettings[type] = 59
+        }
+        if(newSettings[type] < 1) {
+            newSettings[type] = 1
+        }
+        if(newSettings.session <= newSettings.break) {
+            newSettings.session = newSettings.break +1;
+        }
+        
+        setSettings(newSettings)
+    }
+    
+    return (
+        <div className="Tomato">
+        <h2 className="title">Session Length</h2>
+        <div className="setting-container">
+        <span onClick={()=> editSettings('session', -1)}>-</span>
+        <div>{settings.session}</div>
+        <span onClick={()=> editSettings('session', 1)}>+</span>
+        </div>
+        <h2 className="title">Break Length</h2>
+        <div className="setting-container">
+        <span onClick={()=> editSettings('break', -1)}>-</span>
+        <div>{settings.break}</div>
+        <span onClick={()=> editSettings('break', +1)}>+</span>
+        </div>
+        <h2 className="title">Session</h2>
+        <div className="session-container">
+        <div>value</div>
+        </div>
+        <div className='controls-container'>
+        <span>Play</span>
+        <div>Pause</div>
+        <span>Restart</span>
+        </div>
+        </div>
+        );
+    }
+    
+    export default Tomato;
+    
