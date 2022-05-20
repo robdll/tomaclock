7b59321467bd213daff52e2b733834a15d975572
roberto di lillo <dilillo.roberto@gmail.com>
2022-05-20 22:17:12 +0100

    complete fe

--- src/components/Tomato/Tomato.js
+++ src/components/Tomato/Tomato.js
@@ -1,50 +1,69 @@
 import './Tomato.css';
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faPause, faArrowsRotate, faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 function Tomato() {
-    
-    const [settings, setSettings] = useState({session: 25, break: 5});
-    const [timer, setTimer] = useState(25);
-    
-    const editSettings =  (type, val) => {
-        const newSettings = { ...settings }
-        newSettings[type] +=val;
-        if(newSettings[type] > 60) {
-            newSettings[type] = 60
-        }
-        if(newSettings[type] < 1) {
-            newSettings[type] = 1
-        }
-        
-        setSettings(newSettings)
-    }
-    
-    return (
-        <div className="Tomato">
-        <h2 className="title">Session Length</h2>
-        <div className="setting-container">
-        <span onClick={()=> editSettings('session', -1)}>-</span>
-        <div>{settings.session}</div>
-        <span onClick={()=> editSettings('session', 1)}>+</span>
-        </div>
-        <h2 className="title">Break Length</h2>
-        <div className="setting-container">
-        <span onClick={()=> editSettings('break', -1)}>-</span>
-        <div>{settings.break}</div>
-        <span onClick={()=> editSettings('break', +1)}>+</span>
-        </div>
-        <h2 className="title">Session</h2>
-        <div className="session-container">
-        <div>value</div>
-        </div>
-        <div className='controls-container'>
-        <span>Play</span>
-        <div>Pause</div>
-        <span>Restart</span>
-        </div>
-        </div>
-        );
-    }
-    
-    export default Tomato;
-    
+	
+	const [settings, setSettings] = useState({session: 25, break: 5});
+	const [timer, setTimer] = useState({minutes: '25', seconds: '00'});
+	
+	const editSettings =  (type, val) => {
+		const newSettings = { ...settings }
+		newSettings[type] +=val;
+		if(newSettings[type] > 60) {
+			newSettings[type] = 60
+		}
+		if(newSettings[type] < 1) {
+			newSettings[type] = 1
+		}
+		setSettings(newSettings)
+	}
+	
+	return (
+		<div className="Tomato">
+			
+			<h2 className="title">Session</h2>
+			<div className="session-container">
+				<div className='session-value'>{timer.minutes}:{timer.seconds}</div>
+				<div className='controls-container'>
+					<span className='icon-container'>
+						<FontAwesomeIcon icon={faPlay} />
+					</span>
+					<span className='icon-container'>
+						<FontAwesomeIcon icon={faPause} />
+					</span>
+					<span className='icon-container'>
+						<FontAwesomeIcon icon={faArrowsRotate} />
+					</span>
+				</div>
+			</div>
+
+			<h2 className="title">Session Length</h2>
+			<div className="setting-container">
+				<span className='icon-container' onClick={()=> editSettings('session', -1)}>
+					<FontAwesomeIcon icon={faArrowDown} />
+				</span>
+				<div className="setting-value">{settings.session}</div>
+				<span className='icon-container' onClick={()=> editSettings('session', 1)}>
+					<FontAwesomeIcon icon={faArrowUp} />
+				</span>
+			</div>
+			
+			<h2 className="title">Break Length</h2>
+			<div className="setting-container">
+				<span className='icon-container' onClick={()=> editSettings('break', -1)}>
+					<FontAwesomeIcon icon={faArrowDown} />
+				</span>
+				<div className="setting-value">{settings.break}</div>
+				<span className='icon-container' onClick={()=> editSettings('break', +1)}>
+					<FontAwesomeIcon icon={faArrowUp} />
+				</span>
+			</div>
+			
+		</div>
+		);
+	}
+	
+	export default Tomato;
+	
