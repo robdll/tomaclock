84ea4e50f39055c6b355e83e472c6157ebc777e9
roberto di lillo <dilillo.roberto@gmail.com>
2022-05-21 02:28:09 +0100

    adds audio

--- src/components/Tomato/Tomato.js
+++ src/components/Tomato/Tomato.js
@@ -65,12 +65,14 @@ function Tomato() {
 	}
 
 	const refreshTimer = () => {
+		audRef.current.pause();
 		setTimer({ 
-			minutes: settings.session,
+			minutes: '25',
 			seconds: '00', 
 			active: false,
 			isBreak: false
 		})
+		setSettings({session: 25, break: 5});
 	}
 
 	const editSettings =  (type, val) => {
@@ -94,47 +96,48 @@ function Tomato() {
 		
 	}
 
+	let icon = !timer.active ? faPlay : faPause;
+
 	return (
 		<div className="Tomato">
 			
-			<h2 className="title">{settings.isBreak ? 'Break' : 'Session'}</h2>
+			<h2 className="title" id="timer-label">{settings.isBreak ? 'Break' : 'Session'}</h2>
 			<div className="session-container">
-				<div className='session-value'>{timer.minutes}:{timer.seconds}</div>
+				<div className='session-value' id="time-left">{timer.minutes}:{timer.seconds}</div>
 				<div className='controls-container'>
 					<span className='icon-container'>
-						<FontAwesomeIcon onClick={()=>{ toggleTimer(true)}} icon={faPlay} />
+						<FontAwesomeIcon id="start_stop" onClick={()=>{ toggleTimer(!timer.active)}} icon={icon} />
 					</span>
 					<span className='icon-container'>
-						<FontAwesomeIcon onClick={()=>{ toggleTimer(false)}} icon={faPause} />
 					</span>
 					<span className='icon-container'>
-						<FontAwesomeIcon onClick={()=>{ refreshTimer()}} icon={faArrowsRotate} />
+						<FontAwesomeIcon id="reset" onClick={()=>{ refreshTimer()}} icon={faArrowsRotate} />
 					</span>
 				</div>
 			</div>
 
-			<h2 className="title">Session Length</h2>
+			<h2 className="title" id="session-label">Session Length</h2>
 			<div className="setting-container">
-				<span className='icon-container' onClick={()=> editSettings('session', -1)}>
+				<span className='icon-container' id="session-decrement" onClick={()=> editSettings('session', -1)}>
 					<FontAwesomeIcon icon={faArrowDown} />
 				</span>
-				<div className="setting-value">{settings.session}</div>
-				<span className='icon-container' onClick={()=> editSettings('session', 1)}>
+				<div className="setting-value" id="session-length">{settings.session}</div>
+				<span className='icon-container' id="session-decrement" onClick={()=> editSettings('session', 1)}>
 					<FontAwesomeIcon icon={faArrowUp} />
 				</span>
 			</div>
 			
-			<h2 className="title">Break Length</h2>
+			<h2 className="title" id="break-label">Break Length</h2>
 			<div className="setting-container">
-				<span className='icon-container' onClick={()=> editSettings('break', -1)}>
+				<span className='icon-container' id="break-decrement" onClick={()=> editSettings('break', -1)}>
 					<FontAwesomeIcon icon={faArrowDown} />
 				</span>
 				<div className="setting-value">{settings.break}</div>
-				<span className='icon-container' onClick={()=> editSettings('break', +1)}>
+				<span className='icon-container' id="break-increment" onClick={()=> editSettings('break', +1)}>
 					<FontAwesomeIcon icon={faArrowUp} />
 				</span>
 			</div>
-			<audio controls src={alarm} ref={audRef} hidden />
+			<audio id="beep" src={alarm} ref={audRef} hidden />
 			</div>
 		);
 	}
